3a02038cf69cc79e9a28c9302b2cdb07e7545d1c
helloyashdagar <yashdagar576@gmail.com>
2026-03-09 10:11:02 +0530

    file using switch

---
+++ jsbasics/script.js
@@ -0,0 +1,24 @@
+console.log("hello world");
+document.write("hello");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num-false;
+let b="name";
+console.log(b);
+console.log(typeof(b));
+let c=null;
+console.log(c);
+console.log(typeof(c));
+
+ const sym1 =symbol(4);
+ console.log(sym1);
+ const sym2 =symbol(4);
+ console.log(sym2);
+ if(sym1==sym2){
+    console.log("true");
+
+ }
+ else{
+    console.log("false");
+ }
