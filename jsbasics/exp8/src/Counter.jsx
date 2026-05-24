f26845588fe8d342a9cd4babd69ffafe2bed7d3a
helloyashdagar <yashdagar576@gmail.com>
2026-05-24 15:51:21 +0530

    files for remaining experiments

---
+++ jsbasics/exp8/src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Counter.css";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="container">
+            <h1>Counter App</h1>
+            <h2>{count}</h2>
+
+            <button onClick={() => setCount(count + 1)}>
+                Increment
+            </button>
+
+            <button onClick={() => setCount(count - 1)}>
+                Decrement
+            </button>
+
+            <button onClick={() => setCount(0)}>
+                Reset
+            </button>
+        </div>
+    );
+}
+
+export default Counter;
